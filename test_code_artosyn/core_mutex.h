1c7ae9c82de76555f1d8d80784cf30625c04543a
jialiu <liuchanchen@live.com>
2016-11-01 09:20:20 +0800

    更改core_mutex_uninit声明与定义不一致的问题

--- test_code_artosyn/core_mutex.h
+++ test_code_artosyn/core_mutex.h
@@ -14,7 +14,7 @@ extern int test_core_mutex();
 extern int core_mutex_init();
 extern int core_mutex_lock(CORE_MUTEX_ID core_mutex_id);
 extern int core_mutex_unlock(CORE_MUTEX_ID core_mutex_id);
-extern int core_mutex_uninit();
+extern int core_mutex_uninit(CORE_MUTEX_ID core_mutex_id);
 
 #endif
 
